1a53f2284c404e1a4266648e54f849d02885f66a
Trish Z <trishzag@gmail.com>
2015-11-01 23:37:44 -0500

    triangles complete

--- triangle.rb
+++ triangle.rb
@@ -14,13 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && b == c
-    return :equilateral
-  elsif a == b && a != c && b != c || a != b && a == c && b != c || a != b && a != c && b == c
-    return :isosceles
-  elsif a != b && a != b && b != c
-    return :scalene
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError
   end
+  if a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError
+  end
+    if a == b && b == c
+      return :equilateral
+    elsif a == b && a != c && b != c || a != b && a == c && b != c || a != b && a != c && b == c
+      return :isosceles
+    elsif a != b && a != c && b != c
+      return :scalene
+    end
 end
 
 # Error class used in part 2.  No need to change this code.
